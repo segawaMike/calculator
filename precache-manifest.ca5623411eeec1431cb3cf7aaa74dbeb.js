5f1dc908c546cbc3c67e989b99cbc58986553691
segawaMike <collinsmicheal256@gmail.com>
2020-08-21 17:56:58 +0300

    Updates

---
+++ precache-manifest.ca5623411eeec1431cb3cf7aaa74dbeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df6fd220203332dc1bafbe96d98f04f",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "279319b77798d11c9c01",
+    "url": "/calculator/static/css/main.9ab85748.chunk.css"
+  },
+  {
+    "revision": "0a4a8dd5094244bdacf2",
+    "url": "/calculator/static/js/2.31b68df1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.31b68df1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "279319b77798d11c9c01",
+    "url": "/calculator/static/js/main.0cf758c0.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
